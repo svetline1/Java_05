25748e3d5f5f34217d4aed1eefc83bd108e1c83f
unknown <sveto4ka@list.ru>
2022-10-21 11:34:54 +0300

    CountOddEvenValuesInArray methods and Tests added

--- src/main/java/OddEvenValuesInArray.java
+++ src/main/java/OddEvenValuesInArray.java
@@ -4,19 +4,38 @@ public class OddEvenValuesInArray {
     и возвращает количество четных чисел в этом массиве
     */
 
+    public int countEvenValuesInArray(int array[]) {
 
+        int counter = 0;
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                counter +=1;
+            }
+        }
+
+        return counter;
+    }
 
 
 
     /*Написать метод countOddValuesInArray(), который принимает на вход массив целых чисел,  и возвращает
     количество нечетных чисел в этом массиве
 
-    В классе ManipulationsWithArrays написать метод areValuesGreaterThanNumber(), который принимает на вход
-     массив целых чисел и число number. Метод возвращает значение true, если все элементы массива больше number,
-      иначе возвращает false
-
      */
+
+    public int countOddValuesInArray(int array[]) {
+
+        int counter = array.length;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                counter -= 1;
+            }
+        }
+
+        return counter;
+    }
 
 
 
--- src/test/java/OddEvenValuesInArrayTest.java
+++ src/test/java/OddEvenValuesInArrayTest.java
@@ -1,17 +1,47 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class OddEvenValuesInArrayTest {
 
      /*Написать метод countEvenValuesInArray(), который принимает на вход массив целых чисел,
     и возвращает количество четных чисел в этом массиве
+     */
+
+    @Test
+    public void testCountEvenValuesInArray_HappyPath() {
+        //AAA
+        //arrange
+
+        int[] array = {1, 2, 3, 4, 5};
+        int expectedResult = 2;
+
+        //act
+        int actualResult = new OddEvenValuesInArray().countEvenValuesInArray(array);
+
+        //assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 
 
-    Написать метод countOddValuesInArray(), который принимает на вход массив целых чисел,  и возвращает
+/*Написать метод countOddValuesInArray(), который принимает на вход массив целых чисел,  и возвращает
     количество нечетных чисел в этом массиве
 
-    В классе ManipulationsWithArrays написать метод areValuesGreaterThanNumber(), который принимает на вход
-     массив целых чисел и число number. Метод возвращает значение true, если все элементы массива больше number,
-      иначе возвращает false
-
      */
+
+    @Test
+    public void testCountOddValuesInArray_HappyPath() {
+        //AAA
+        //arrange
+
+        int[] array = {1, 2, 3, 4, 5};
+        int expectedResult = 3;
+
+        //act
+        int actualResult = new OddEvenValuesInArray().countOddValuesInArray(array);
+
+        //assert
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 
 
 
